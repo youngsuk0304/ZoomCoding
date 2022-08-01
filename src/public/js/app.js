6d9d288196a2438a7782ddb7d4d4b18544749ba3
Youngsuk <djfls0304@naver.com>
2022-08-01 22:40:32 +0900

    WebSoket Events

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,32 @@
-alert("hi");
+//alert("hi");
+//front 에서 socket과 연결하는 부분
+const socket = new WebSocket("ws://localhost:3000");
+//const socket = new WebSocket('ws://${window.location.host}');
+//app.js의 socket은 서버로의 연결을 의미
+
+//open, message, error, close 가능
+socket.addEventListener("open",()=>{
+  console.log("Connected to Brower O");
+});
+
+//브라우저가 무슨 메세지를 받았을때
+socket.addEventListener("message",(message)=>{
+  //Juset got this : ",message.data,"from the Server");
+  const translatedMessageData = message.toString('utf8');
+
+  console.log("New message : ",message.data,"from the Server");
+
+});
+
+//서버가 오프라인 됐을때
+socket.addEventListener("close",()=>{
+  console.log("Disconnected to Brower X");
+});
+
+
+setTimeout(()=>{
+  socket.send("hello from the browser!");
+},5000);
+//예정은 hello from the broewer! 이라는 message가 보내져야하고 
+//server.js에서 socket.on(message)에서 hello from the broewer!를 받아 출력해줘야한다.
+//하지만 <Buffer 68 65 6c 6c 6f 20 66 72 6f 6d 20 74 68 65 20 62 72 6f 77 73 65 72 21>값이 출력된다.
--- src/server.js
+++ src/server.js
@@ -31,6 +31,32 @@ const wss = new WebSocket.Server({server});
 //localhost:3000 은 http 그리고 ws 둘다 동작
 //설명 http 서버 위에 webSocket 서버를 만듬
 
+//server.js의 socket은 연결된 브라우저를 의미
+// function handleConnection(socket){
+//   console.log(socket);
+// }
+//wss의 click, submit, wifi on/off 처렴 신호connection를 주는 부분
+//wss.on("connection",handleConnection);
+//위의 방법이 아닌 function안에 기능들을 넣는게 생각하기 더 좋기 때문에
+//connection 안에 같은 역할을 하는 익명함수를 만듬
+//event를 다룰 때도 아래방식이 좋음
+
+//backend에서 connection을 기다리는 부분
+wss.on("connection",(socket)=>{
+  //console.log(socket);
+  console.log("Connected to Brower");
+  //브라우져가 꺼졌을때
+  socket.on("close",()=>console.log("Disconnected to Brower X" ));
+  //브라우저가 서버에 메세지를 보냈을때
+  socket.on("message",(message)=>{
+    const translatedMessageData = message.toString('utf8');
+    console.log(translatedMessageData);
+  });
+  //브라우저에 메세지 보내기
+  socket.send("hello!!!");
+});
+//connection이 오면 socket을 받는다는것을 알 수 있다.
+
 server.listen(3000,handleListen);
 //app.lister(3000,handleListen)과 다른점 . 내 http서버에 access 한다.
 //그래서 http 서버 위에 webSocket 서버를 만들 수 있게 했다.
